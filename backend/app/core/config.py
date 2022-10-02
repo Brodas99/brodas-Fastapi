a3a2ecb003382bfa97128134628554dee8853f90
Bryan Rodas <B.rodasdiaz@gmail.com>
2022-10-02 17:38:34 -0500

    recasting env vars in starlette config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,3 @@
-import string
-
-from numpy import real
 import databases
 
 from starlette.applications import Starlette
@@ -9,7 +6,7 @@ from starlette.datastructures import CommaSeparatedStrings, Secret
 
 
 # Config will be read from environment variables and/or ".env" files.
-config = Config(".env")
+config = Config("api.env")
 
 PROJECT_NAME = "cbh_api"
 VERSION = "1.0.0"
@@ -18,12 +15,12 @@ API_PREFIX = "/api"
 SECRET_KEY = config("SECRET_KEY", cast=Secret, default="CHANGEME")
 
 DEBUG = config('DEBUG', cast=bool, default=False)
-POSTGRES_USER = config('POSTGRES_USER', cast=string, default=False)
-POSTGRES_PASSWORD = config('POSTGRES_PASSWORD', cast=string, cast=Secret)
-POSTGRES_SERVER= config('POSTGRES_SERVER', cast=string, default="db")
-POSTGRES_HOST = config('POSTGRES_HOST', cast=real, default=False)
+POSTGRES_USER = config('POSTGRES_USER', cast=str, default=False)
+POSTGRES_PASSWORD = config('POSTGRES_PASSWORD', cast=Secret)
+POSTGRES_SERVER= config('POSTGRES_SERVER', cast=str, default="db")
+POSTGRES_HOST = config('POSTGRES_HOST', cast=str, default=False)
 POSTGRES_PORT = config('POSTGRES_PORT', cast=int, default="5435")
-POSTGRES_DB= config('POSTGRES_DB', cast=string, default=False)
+POSTGRES_DB= config('POSTGRES_DB', cast=str, default=False)
 DATABASE_URL = config(
     'DATABASE_URL', 
     cast=databases.DatabaseURL,
